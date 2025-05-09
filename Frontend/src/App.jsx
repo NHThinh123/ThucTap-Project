5262df8f2942f8955f7448ee9dd435fbf14ef992
Thinh <155166003+NHThinh123@users.noreply.github.com>
2025-05-09 09:00:18 +0700

    header

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,27 +1,43 @@
 import { Outlet, Link } from "react-router-dom";
-import { Card } from "antd";
-function App() {
-  return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/video">Video</Link>
-          </li>
+import { Layout, Menu } from "antd";
 
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/signup">Signup</Link>
-          </li>
-        </ul>
-      </nav>
-      <Outlet />
-    </>
+const { Header, Content } = Layout;
+
+function App() {
+  const menuItems = [
+    { key: "home", label: <Link to="/">Home</Link> },
+    { key: "video", label: <Link to="/video">Video</Link> },
+    { key: "login", label: <Link to="/login">Login</Link> },
+    { key: "signup", label: <Link to="/signup">Signup</Link> },
+    { key: "channel", label: <Link to="/channel">Channel</Link> },
+  ];
+
+  return (
+    <Layout>
+      <div
+        style={{
+          position: "fixed",
+          zIndex: 1000,
+          width: "100%",
+        }}
+      >
+        <Menu
+          mode="horizontal"
+          defaultSelectedKeys={["home"]}
+          items={menuItems}
+          style={{ flex: 1, minWidth: 0 }}
+        />
+      </div>
+      <Content
+        style={{
+          padding: "24px",
+          marginTop: "32px",
+          minHeight: "calc(100vh - 32px)",
+        }}
+      >
+        <Outlet />
+      </Content>
+    </Layout>
   );
 }
 
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,12 +2,14 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./styles/global.css";
 import App from "./App.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import VideoDetailPage from "./pages/VideoDetailPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 
 import SignupPage from "./pages/SignupPage.jsx";
+import ChannelPage from "./pages/ChannelPage.jsx";
 
 const queryClient = new QueryClient();
 
@@ -33,6 +35,10 @@ const router = createBrowserRouter([
         //     element: <PostDetailPage />,
         //   },
         // ],
+      },
+      {
+        path: "channel",
+        element: <ChannelPage />,
       },
     ],
   },
